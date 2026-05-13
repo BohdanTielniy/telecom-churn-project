f111fc1fa5180bf194f3eab8f2992f9b4858bec4
Bohdan Tielniy <bogdan.tlny@gmail.com>
2026-05-13 04:01:20 +0300

    Created config-manager package

---
+++ src/config.py
@@ -0,0 +1,51 @@
+import yaml
+from pathlib import Path
+from dataclasses import dataclass
+
+@dataclass
+class SourceConfig:
+    kaggle_dataset: str
+    raw_file_name: str
+
+@dataclass
+class PathsConfig:
+    raw_data_dir: Path
+    processed_data_dir: Path
+    log_file: Path
+
+@dataclass
+class TransformParams:
+    target_column: str
+    charges_column: str
+
+class ProjectConfig:
+    """Клас для централізованого управління конфігураціями проекту."""
+    
+    def __init__(self, config_path: str = "config/etl_config.yaml"):
+        self.config_path = Path(config_path)
+        self._config_data = self._load_yaml()
+        
+        # Ініціалізація підконфігів для зручного доступу
+        self.source = SourceConfig(**self._config_data['source'])
+        self.paths = PathsConfig(
+            raw_data_dir=Path(self._config_data['paths']['raw_data_dir']),
+            processed_data_dir=Path(self._config_data['paths']['processed_data_dir']),
+            log_file=Path(self._config_data['paths']['log_file'])
+        )
+        self.transform = TransformParams(**self._config_data['transform_params'])
+
+    def _load_yaml(self) -> dict:
+        """Приватний метод для зчитування YAML файлу."""
+        try:
+            with open(self.config_path, 'r', encoding='utf-8') as file:
+                return yaml.safe_load(file)
+        except FileNotFoundError:
+            raise Exception(f"Конфігураційний файл не знайдено за шляхом: {self.config_path}")
+        except yaml.YAMLError as exc:
+            raise Exception(f"Помилка при читанні YAML: {exc}")
+
+    def create_dirs(self):
+        """Метод для автоматичного створення необхідних папок (data/raw тощо)."""
+        self.paths.raw_data_dir.mkdir(parents=True, exist_ok=True)
+        self.paths.processed_data_dir.mkdir(parents=True, exist_ok=True)
+        self.paths.log_file.parent.mkdir(parents=True, exist_ok=True)
