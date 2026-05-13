0bac7fc1fa01c761471cf616276318b0cbd8055c
Bohdan Tielniy <bogdan.tlny@gmail.com>
2026-05-13 13:03:27 +0300

    ETL: extract pipline relialized

--- README.md
+++ README.md
@@ -18,3 +18,16 @@ source .venv/bin/activate
 ```bash
 pip install -r requirements.txt
 ```
+3. **Створіть файл kaggle.json**
+
+- Увійдіть в свій акаунт на [Kaggle](https://www.kaggle.com/)
+- Натисніть на фото свого профілю у верхньому правому куті та виберіть **Settings**.
+- Оберіть розділ **API Tokens**.
+- Натисніть кнопку **Generate New Token**.
+- Назвіть його ``kagle``
+- Збережіть його в ``~/.kaggle/access_token``
+```bash
+mkdir -p ~/.kaggle
+echo *token_name* > ~/.kaggle/access_token
+chmod 600 ~/.kaggle/access_token
+```
---
+++ src/data/extract.py
@@ -0,0 +1,63 @@
+import logging
+import json
+from datetime import datetime
+from pathlib import Path
+from kaggle.api.kaggle_api_extended import KaggleApi
+from src.config import ProjectConfig
+
+logger = logging.getLogger(__name__)
+
+class DataExtractor:
+    """Клас для завантаження даних з Kaggle."""
+
+    def __init__(self, config: ProjectConfig):
+        self.config = config
+        self.api = KaggleApi()
+        try:
+            self.api.authenticate()
+        except Exception as e:
+            logger.error(f"Помилка авторизації Kaggle API: {e}")
+            raise
+
+    def extract(self) -> Path:
+        """
+        Завантажує датасет, фіксує час у метаданих та зберігає у raw директорію.
+        """
+        dataset = self.config.source.kaggle_dataset
+        raw_dir = self.config.paths.raw_data_dir
+        
+        logger.info(f"Початок завантаження датасету: {dataset}")
+
+        try:
+            self.api.dataset_download_files(
+                dataset, 
+                path=raw_dir, 
+                unzip=True
+            )
+            
+            raw_file_path = raw_dir / self.config.source.raw_file_name
+            metadata_path = raw_dir / self.config.source.metadata_file_name
+            extraction_time = datetime.now().isoformat()
+            metadata = {
+                    "source": dataset,
+                    "extraction_time": extraction_time,
+                    "file_saved_at": str(raw_file_path),
+                }
+            
+            if raw_file_path.exists():
+                metadata.status = "Success"
+                with open(metadata_path, "w", encoding="utf-8") as meta_file:
+                    json.dump(metadata, meta_file, indent=4)
+                
+                logger.info(f"Дані успішно завантажені. Метадані зафіксовано у {metadata_path}")
+                return raw_file_path
+            else:
+                metadata.status = "Failed"
+                with open(metadata_path, "w", encoding="utf-8") as meta_file:
+                    json.dump(metadata, meta_file, indent=4)
+                raise FileNotFoundError(f"Файл {self.config.source.raw_file_name} не знайдено.")
+
+
+        except Exception as e:
+            logger.error(f"Критична помилка під час етапу Extract: {e}", exc_info=True)
+            raise
